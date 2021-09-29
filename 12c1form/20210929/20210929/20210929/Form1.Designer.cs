98f9f980f3c4f5607c1251fb0e9e0b58fe36338c
szerdahelyigergely13 <43171800+szerdahelyigergely13@users.noreply.github.com>
2021-09-29 12:29:45 +0200

    Add files via upload

---
+++ 12c1form/20210929/20210929/20210929/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace _20210929
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblhonap = new System.Windows.Forms.Label();
+            this.txtsorszam = new System.Windows.Forms.TextBox();
+            this.lblevszak = new System.Windows.Forms.Label();
+            this.btnevszak = new System.Windows.Forms.Button();
+            this.btnkilep = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblhonap
+            // 
+            this.lblhonap.AutoSize = true;
+            this.lblhonap.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblhonap.Location = new System.Drawing.Point(7, 30);
+            this.lblhonap.Name = "lblhonap";
+            this.lblhonap.Size = new System.Drawing.Size(129, 16);
+            this.lblhonap.TabIndex = 0;
+            this.lblhonap.Text = "Hónapok sorszáma:";
+            // 
+            // txtsorszam
+            // 
+            this.txtsorszam.Location = new System.Drawing.Point(142, 26);
+            this.txtsorszam.Name = "txtsorszam";
+            this.txtsorszam.Size = new System.Drawing.Size(100, 20);
+            this.txtsorszam.TabIndex = 1;
+            this.txtsorszam.TextChanged += new System.EventHandler(this.Txtsorszam_TextChanged);
+            // 
+            // lblevszak
+            // 
+            this.lblevszak.AutoSize = true;
+            this.lblevszak.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblevszak.Location = new System.Drawing.Point(93, 96);
+            this.lblevszak.Name = "lblevszak";
+            this.lblevszak.Size = new System.Drawing.Size(48, 13);
+            this.lblevszak.TabIndex = 2;
+            this.lblevszak.Text = "Évszak";
+            // 
+            // btnevszak
+            // 
+            this.btnevszak.Location = new System.Drawing.Point(12, 208);
+            this.btnevszak.Name = "btnevszak";
+            this.btnevszak.Size = new System.Drawing.Size(75, 23);
+            this.btnevszak.TabIndex = 3;
+            this.btnevszak.Text = "Évszak";
+            this.btnevszak.UseVisualStyleBackColor = true;
+            this.btnevszak.Click += new System.EventHandler(this.Btnevszak_Click);
+            // 
+            // btnkilep
+            // 
+            this.btnkilep.Location = new System.Drawing.Point(167, 208);
+            this.btnkilep.Name = "btnkilep";
+            this.btnkilep.Size = new System.Drawing.Size(75, 23);
+            this.btnkilep.TabIndex = 4;
+            this.btnkilep.Text = "Kilépés";
+            this.btnkilep.UseVisualStyleBackColor = true;
+            this.btnkilep.Click += new System.EventHandler(this.Btnkilep_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(254, 243);
+            this.Controls.Add(this.btnkilep);
+            this.Controls.Add(this.btnevszak);
+            this.Controls.Add(this.lblevszak);
+            this.Controls.Add(this.txtsorszam);
+            this.Controls.Add(this.lblhonap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblhonap;
+        private System.Windows.Forms.TextBox txtsorszam;
+        private System.Windows.Forms.Label lblevszak;
+        private System.Windows.Forms.Button btnevszak;
+        private System.Windows.Forms.Button btnkilep;
+    }
+}
+
---
+++ 12c1form/20210929/20210929/20210929/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20210929
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnkilep_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Txtsorszam_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnevszak_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtsorszam.Text);
+            if (a<3 || a==12)
+            {
+                lblevszak.Text = "Tél";
+            }
+            else if (a<6)
+            {
+                lblevszak.Text = "Tavasz";
+            }
+            else if (a < 9)
+            {
+                lblevszak.Text = "Nyár";
+            }
+            else
+            {
+                lblevszak.Text = "Ősz";
+            }
+            
+
+
+        }
+    }
+}
